d6519b3a7adaee6aa3e2dd797df871f08805ea03
Damian Vicens <damianvicens@gmail.com>
2022-06-14 20:06:36 -0300

    Avanzado

--- Coder_house/Tps/preproyecto/prefinal.js
+++ Coder_house/Tps/preproyecto/prefinal.js
@@ -1,4 +1,14 @@
-//Objeto de tipo de Articulos
+//Declaracion de Arrays a utilizar
+const detCompra = []; 
+const detallePedido = [];
+     
+     
+     
+ // solicitar  datos 
+ let nombre = prompt ("Ingrese su Nombre")
+  
+  
+//Objeto  Articulo
 class articulo 
 {
     constructor (codArt,nomArt,preArt)
@@ -7,82 +17,48 @@ class articulo
     this.nomArt = nomArt ;
     this.preArt = preArt ;
     }
-    mostrarArticulo (){}
+    mostrarArticulo (){};      
 }
-// precarga de articulos
+//precarga de articulos
 const art1 = new articulo ("extra liviano", "SAW 05-30", 500)
 const art2 = new articulo ("liviano", "SAW 10-40", 450)
 const art3 = new articulo ("Mediano", "SAW 15-40", 400)
-const art4 = new articulo ("pesado", "SAW 25-50", 350)
+const art4 = new articulo ("pesado", "SAW 25-50", 350) 
+        
 
-// Objetos detalle de pedido
-class detallePedido
-{
-    constructor (articulo, cantidad )
-    {
-        this.articulo = articulo
-        this.cantidad = cantidad
-    }
-    calcularSubtotal(){
-        let subtotal = this.articulo.preArt * this.cantidad 
-        return subtotal       
-    }
-    mostrarDetalle(){}
-}
+// funcion solitar articulo 
 
-//precarga de Detalle pedido
-const pedido1 = new detallePedido (art1, 10);
-const pedido2 = new detallePedido (art2, 15);
-const pedido3 = new detallePedido (art3, 20);
-const pedido4 = new detallePedido (art4, 25);
-
-// declaracion Pedido Final
-
-class pedidoFinal {
-    constructor(fecha, detalles) {
-        this.fecha = fecha;
-        this.detalles = detalles;
-    }
-    calcularTotal()
-    {
-        let total = 0;
-        for(const dp of this.detalles)
-        {
-            total = total + dp.calcularSubtotal();
-        }
-        return total;
-    }
-    mostrarPedido (){}
-}
-
-// Carga de Array toal de pedidos 
-const totalPedidos = [];
-totalPedidos.push(pedido1);
-totalPedidos.push(pedido2);
-totalPedidos.push(pedido3);
-totalPedidos.push(pedido4);
-
-const costoFinal = new pedidoFinal(new Date(), totalPedidos);
-const artListados =  [art4,art2,art3,art1]
-
-
-
- console .log ("--lista de articulos desordenada--")
-console.log (artListados)
-
-
-// ordenamiento de articulos 
-artListados.sort ((a,b) => {
-    
-    if (a.nomArt > b.nomArt)
-    { return 1 }
-    if (a.nomArt < b.nomArt)
-    {return -1}
-    return 0;   })
-
-console.log ("--Articulos Ordenados alfabeticamente--")
-console.log (artListados) 
-
-
-console.log ("Articulo mas barato")
- math.min(articulo.preArt)
+          function solArticulo (){
+          {
+           artSol = prompt ( nombre +"  "+ "Elija su tipo de aceite"+"\n"+ "1 = extra liviano"+"\n"+ "2 = liviano" +"\n" + "3 = mediano"+ "\n" + "4 =pesado ")
+              switch  (artSol)
+              {
+               case "1":
+                artSol = art1.preArt
+                alert ("el valor del SAW  5-30 es:" + artSol);                
+               break;
+               case "2": 
+                articulo = 450
+                alert ("el valor del SAW  10-40 es:" + articulo)   
+               break;
+               case "3":
+                articulo  = 400
+                alert ("el valor del SAW  15-40 es:" + articulo)      
+               break;
+               case  "4":
+                 articulo = 350
+                 alert ("el valor del SAW  25-50 es:" + articulo)         
+              break;
+              default :
+              alert ( "Dato no valido") 
+              solArticulo ()
+              }
+            }
+        }     
+     
+      solArticulo ();
+      
+      let litro = prompt ("Ingrese la cantidad de litros que desea")
+      alert ("Estimado" + nombre + "Su total a pagar es:  " + (Artsol * litro))
+      alert ("Muchas Gracias por su compra")
+      close ( )
